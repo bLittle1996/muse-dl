715aaa87d3c5adeaabb687ca34795d021b7895cf
Ben Littleton <b.little1996@gmail.com>
2020-10-22 20:28:49 -0400

    wip bundling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log("henlo");
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ color: 'green' }, () => {
+        console.log("Color set to greeeeen")
+    })
+})
